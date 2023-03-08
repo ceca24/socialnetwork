e3f91bf77b6b3d400c8560d6401c2e88a535d889
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-07 23:08:14 -0600

    created thoughtSchema section in Thought.js in models folder

--- models/Thought.js
+++ models/Thought.js
@@ -31,3 +31,35 @@ const ReactionSchema = new Schema(
         }
     }
 );
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+            required: true,
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
